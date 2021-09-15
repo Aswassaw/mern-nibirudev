1b524397461007ca9430c43cef6ce6452f186ceb
Andry Pebrianto <andrypeb227@gmail.com>
2021-09-16 01:39:45 +0700

    Connecting To MongoDB With Mongoose

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const config = require("config");
 
 const app = express();
+
+// Connecting Database
+connectDB();
 
 app.use("/", (req, res) => {
   res.send("Hello World");
@@ -11,4 +16,5 @@ const PORT = process.env.PORT || 4500;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
   console.log(`Visit http://localhost:${PORT}`);
+  console.log(`Developed by ${config.get("author")}`);
 });
