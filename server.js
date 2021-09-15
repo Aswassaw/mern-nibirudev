8d3da428b3015776c5087335a91d5a3752eaebac
Andry Pebrianto <andrypeb227@gmail.com>
2021-09-16 01:55:56 +0700

    Route Files With Express Router

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
-const connectDB = require("./config/db");
+// const connectDB = require("./config/db");
 const config = require("config");
 
 const app = express();
 
 // Connecting Database
-connectDB();
+// connectDB();
 
-app.use("/", (req, res) => {
-  res.send("Hello World");
-});
+// Root endpoint
+app.get("/", (req, res) => res.send("Hello World"));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 4500;
-
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
   console.log(`Visit http://localhost:${PORT}`);
