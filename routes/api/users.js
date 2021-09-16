46936360fa563be9ed1a986583dc61b407a143de
Andry Pebrianto <andrypeb227@gmail.com>
2021-09-16 08:05:44 +0700

    Request & Body Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const { validationResult } = require("express-validator");
+const { registerValidation } = require("../../validation/users");
 
-// @route  | GET api/users
-// @desc   | Test route
+// @route  | POST api/users
+// @desc   | Endpoint untuk mendaftarkan user baru
 // @access | Public
-router.get("/", (req, res) => {
-  res.send("User router");
+router.post("/", registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json(errors);
+
+  res.json(req.body);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Connecting Database
 // connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 // Root endpoint
 app.get("/", (req, res) => res.send("Hello World"));
 
