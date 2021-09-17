d65e73991dbe032a0753fff7f37b7503189f9f22
Andry Pebrianto <andrypeb227@gmail.com>
2021-09-18 05:35:02 +0700

    Get & Delete Post Routes

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,7 +3,7 @@ const User = require("../models/User");
 const Profile = require("../models/Profile");
 const Post = require("../models/Post");
 
-// Controller untuk endpoint: api/posts
+// Controller untuk endpoint: POST api/posts
 const postNewPost = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) return res.status(400).json(errors);
@@ -27,4 +27,61 @@ const postNewPost = async (req, res) => {
   }
 };
 
-module.exports = { postNewPost };
+// Controller untuk endpoint: GET api/posts
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+// Controller untuk endpoint: GET api/posts/:post_id
+const getPostById = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+};
+
+// Controller untuk endpoint: GET api/posts/:post_id
+const deletePostById = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    // Check user
+    if (post.userId.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not Authorized" });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+};
+
+module.exports = { postNewPost, getAllPosts, getPostById, deletePostById };
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const authMiddleware = require("../../middleware/auth");
-const { postNewPost } = require("../../controllers/posts");
+const {
+  postNewPost,
+  getAllPosts,
+  getPostById,
+  deletePostById,
+} = require("../../controllers/posts");
 const { postNewPostValidation } = require("../../validation/posts");
 
 const router = express.Router();
 
 // @route  | POST api/posts
 // @desc   | Create new post
-// @access | Provate
+// @access | Private
 router.post("/", [authMiddleware, postNewPostValidation], postNewPost);
+
+// @route  | GET api/posts
+// @desc   | Get all posts
+// @access | Private
+router.get("/", authMiddleware, getAllPosts);
+
+// @route  | GET api/posts/:post_id
+// @desc   | Get post by id
+// @access | Private
+router.get("/:post_id", authMiddleware, getPostById);
+
+// @route  | DELETE api/posts/:post_id
+// @desc   | Delete post by id
+// @access | Private
+router.delete("/:post_id", authMiddleware, deletePostById);
 
 module.exports = router;
