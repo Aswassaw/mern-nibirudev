9bc46e0e6c7ac440d20f3e66f0c5257f5f05c373
Andry Pebrianto <andrypeb227@gmail.com>
2021-09-23 04:07:45 +0700

    Register Form & useState hook

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,11 +1,91 @@
-import React from 'react'
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>
-      Register
-    </div>
-  )
-}
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+  const { name, email, password, password2 } = formData;
 
-export default Register
+  const onChangeHandler = (e) =>
+    setFormData((c) => ({
+      ...c,
+      [e.target.name]: e.target.value,
+    }));
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if (password !== password2) {
+      console.log("Password tidak cocok");
+    } else {
+      console.log(formData);
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" onSubmit={onSubmitHandler}>
+        <div className="form-group">
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={name}
+            onChange={onChangeHandler}
+            maxLength="50"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={onChangeHandler}
+            required
+          />
+          <small className="form-text">
+            This site uses Gravatar, so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={onChangeHandler}
+            minLength="8"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            name="password2"
+            placeholder="Confirm Password"
+            value={password2}
+            onChange={onChangeHandler}
+            required
+          />
+        </div>
+        <input type="submit" value="Register" className="btn btn-primary" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="login">Login</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Register;
--- validation/auth.js
+++ validation/auth.js
@@ -3,9 +3,12 @@ const { check } = require("express-validator");
 // Validasi untuk endpoint: POST api/auth/register
 const registerValidation = [
   check("name", "Name is required").not().isEmpty(),
+  check("name", "Name maximum length is 50 characters").isLength({
+    max: 50,
+  }),
   check("email", "Please include a valid email").isEmail(),
   check("password", "Password require 6 or more characters").isLength({
-    min: 6,
+    min: 8,
   }),
 ];
 
